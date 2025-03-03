50fd002e602f0e16e570543b6b9a16cdfb86e15f
malaitaic-marcellin <malaitaic@marcellin.school.nz>
2025-03-04 10:46:36 +1300

    Written more code

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 print("Hello, World!")
 print("My favourite colour is yellow.")
+
+name = input("What is your name: ")
+print(f"Kia ora {name}")
+
+age = input("What is your age: ")
+print(f"{name}, did you say you were {age}? You are old.")
